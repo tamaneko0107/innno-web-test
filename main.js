6b1e3334220f6b030e3dbab2df67e10d1ad61618
ChaosOp <tp6ru8bj6@gmail.com>
2023-07-20 14:24:45 +0800

    fixed name rule add func.js minify main.js

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
+(() => {
 
-    document.querySelector('#file-input').addEventListener('change', function(event) {
-        const fileName = event.target.files[0].name;
-        document.querySelector('.custom-file-input span').textContent = fileName;
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+        get('#file_input')[0].addEventListener('change', function (event) {
+            const file_name = event.target.files[0].name;
+            get('.custom_file_input span')[0].textContent = file_name;
+        });
+
+
+
+        const IO_code_convert = {
+            0: "input",
+            1: "output"
+        };
+
+        [0, 1].forEach((IO_code) => {
+            const [this_IO, target_IO] = [IO_code_convert[IO_code], IO_code_convert[+!IO_code]];
+            get(`#${this_IO}_button`)[0].addEventListener('click', (event) => {
+                event.target.disabled = true;
+                get(`#${target_IO}_button`)[0].disabled = false;
+                get(`.content_${this_IO}`)[0].classList.add('open');
+                get(`.content_${target_IO}`)[0].classList.remove('open');
+            });
+
+        });
+
+        get('#input_button')[0].dispatchEvent(new Event("click"));
+
+
+
+
+
     });
 
 
-    document.querySelector('#button2').disabled = true;
-    document.querySelector('.content-output').classList.add('open');
-
-    document.querySelector('#button1').addEventListener('click', function() {
-        this.disabled = true;
-        document.querySelector('#button2').disabled = false;
-        document.querySelector('.content-input').classList.add('open');
-        document.querySelector('.content-output').classList.remove('open');
-    });
-    document.querySelector('#button2').addEventListener('click', function() {
-        this.disabled = true;
-        document.querySelector('#button1').disabled = false;
-        document.querySelector('.content-output').classList.add('open');
-        document.querySelector('.content-input').classList.remove('open');
-    });
-});
+})();
